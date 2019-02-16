c4039c124bf90dc764ad8e1314efc56956892f4f
Gustavo Ames <gustavo.ames87@gmail.com>
2019-02-16 16:15:38 -0200

    implementacao da logica para carregar o menu

--- pedidoPizza/src/app/menu/menu.component.ts
+++ pedidoPizza/src/app/menu/menu.component.ts
@@ -7,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  public TITULO = "Pedido de Pizza";
+  public menu;
+
+  constructor() {
+    this.criarMenu();
+  }
+
+  criarMenu(){
+    this.menu =[
+      {
+        titulo: "Cadastro Basico",
+        itens: [
+          { descricao: "Sabor", link: "sabor.html"},
+          { descricao: "Tamanho", link: "tamanho.html"},
+          { descricao: "Bebida", link: "bebida.html"},
+          { descricao: "Cliente", link: "cliente.html"}
+        ]
+      },
+      {
+        titulo: "Pedido",
+        itens: [
+          { descricao: "Registrar Pedido", link: "pedido_form.html"},
+          { descricao: "Listagem de Pedido", link: "pedido.html"}
+        ]
+      }
+    ]
+  }
 
   ngOnInit() {
   }
